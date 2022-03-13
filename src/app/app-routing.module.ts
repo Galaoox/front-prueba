afcf7a6992c1b98eeba47768f0249168c3bead1c
Erick Vergara <erickandres30@gmail.com>
2022-03-13 16:07:19 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'productos',
+    loadChildren: () => import("@modules/productos/productos.module").then(m => m.ProductosModule)
+  },
+  {
+    path: 'clientes',
+    loadChildren: () => import('@modules/clientes/clientes.module').then(m => m.ClientesModule)
+  },
+  {
+    path: 'ventas',
+    loadChildren: () => import('@modules/ventas/ventas.module').then(m => m.VentasModule)
+  },
+  {
+    path: '',
+    redirectTo: 'productos',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
